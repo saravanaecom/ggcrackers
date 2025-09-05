6f8854cf48bd3a9705810c3bd72ab9775c798c4c
saravanaecom <saravanaecomweb@gmail.com>
2025-09-05 17:54:38 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -44,7 +44,7 @@ export default function HomePage() {
                         }}
                     /> 
                 </Box>
-                <OfferFastMovingProduct/>
+             
                 <BrandSlider />
                 <HeroSection />
           
